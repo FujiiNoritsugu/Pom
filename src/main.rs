715a3cc2dfbe516f32b972fd16cf1a9f86e71973
FujiiNoritsugu <quercus491@gmail.com>
2021-10-22 08:52:53 +0900

    コピーの代わりにRcとRefCellの組み合わせを使う

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,33 @@ mod consts;
 
 use crate::pom::Pom;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
     // 双方向に腹持ちできる構造をつくる。
     // 20211022 コピーをRcを使用して書き直してみる
     // 100件分のリストを作成
-    let mut pom_list:Vec<Rc<Pom>> = Vec::new();
+    let mut pom_list:Vec<Rc<RefCell<Pom>>> = Vec::new();
     for i in 0..100{
-        pom_list.push(Rc::new(Pom::new(i)));
+        pom_list.push(Rc::new(RefCell::new(Pom::new(i))));
     }
 
     // 同じ内容のリストをコピーする
-    let sub_list:Vec<Rc<Pom>> = pom_list.iter().map(|x| Rc::clone(&x)).collect();
+    let sub_list:Vec<Rc<RefCell<Pom>>> = pom_list.iter().map(|x| Rc::clone(&x)).collect();
 
     for pom1 in pom_list.iter_mut(){
         for pom2 in sub_list.iter(){
             // ここのpom2.copy()がダサい→20211022 ここをRcを使用して変更してみる
             // pom2をいれようとすると&pomやからダメと言われる
             // Rcのクローンでいれようとすると「cannot borrow data in an `Rc` as mutable」と言われる
-            pom1.add_list(Rc::clone(pom2));
+            // RcとRefCellの組み合わせでコピーなしでできた。
+            pom1.borrow_mut().add_list(Rc::clone(pom2));
         }
     }
 
     for pom in pom_list.iter(){
         // calc_pointが全部おんなじになるなんでや？
         // →ポイントも乱数で作成すると少しは違う値になった
-        println!("point:{}",pom.calc_point());
+        println!("point:{}",pom.borrow().calc_point());
     }
 }
--- src/pom.rs
+++ src/pom.rs
@@ -1,9 +1,10 @@
 use crate::rand::Rand;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 pub struct Pom{
     id:i32,
-    list:Vec<Rc<Pom>>,
+    list:Vec<Rc<RefCell<Pom>>>,
     point:f32
 }
 
@@ -18,12 +19,12 @@ impl Pom{
         }
     }
 
-    pub fn add_list(&mut self, p:Rc<Pom>){
-        self.list.push(Rc::clone(&p));
+    pub fn add_list(&mut self, p:Rc<RefCell<Pom>>){
+        self.list.push(p);
     }
 
     pub fn calc_point(&self)->f32{
         let mut rng = Rand::new(self.id as u32);
-        self.list.iter().map(|x| x.point * rng.random() as f32).sum()
+        self.list.iter().map(|x| x.borrow().point * rng.random() as f32).sum()
     }
 }
