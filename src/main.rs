7982566f47b986c4b92c7c3d57ce6107844d311b
FujiiNoritsugu <quercus491@gmail.com>
2021-10-22 07:31:36 +0900

    RcとRefCellを使ってリストのコピーをしてみる。

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod pom;
 mod rand;
 mod consts;
 
+use std::rc::Rc;
+use std::cell::RefCell;
 use crate::pom::Pom;
 
 fn main() {
@@ -13,23 +15,24 @@ fn main() {
     pom2.add_list(pom1.copy());
     
     // 次は100件
-    let mut pom_list:Vec<Pom> = Vec::new();
+    let pom_list_rc:Rc<RefCell<Vec<Pom>>> = Rc::new(RefCell::new(Vec::new()));
     for i in 0..100{
-        pom_list.push(Pom::new(i));
+        pom_list_rc.borrow_mut().push(Pom::new(i));
     }
 
     // 同じ内容のリストをコピーする
-    let sub_list:Vec<Pom> = pom_list.iter().map(|x| x.copy()).collect();
-
-    for pom1 in pom_list.iter_mut(){
-        for pom2 in sub_list.iter(){
+    // let sub_list:Vec<Pom> = pom_list.iter().map(|x| x.copy()).collect();
+    let outer_list = Rc::clone(&pom_list_rc);
+    for pom1 in outer_list.borrow_mut().iter_mut(){
+        let inner_list = Rc::clone(&pom_list_rc);
+        for pom2 in inner_list.borrow_mut().iter(){
             // ここのpom2.copy()がダサい
             // pom2をいれようとすると&pomやからダメと言われる
             pom1.add_list(pom2.copy());
         }
     }
 
-    for pom in pom_list.iter(){
+    for pom in pom_list_rc.borrow().iter(){
         // calc_pointが全部おんなじになるなんでや？
         // ポイントも乱数で作成すると少しは違う値になった
         println!("point:{}",pom.calc_point());
